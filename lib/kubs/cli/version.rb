d135ec69cc799b36f1d017246aaf954678fde6c8
paramagicdev <konnor5456@gmail.com>
2019-08-20 21:50:37 -0400

    initial commit

---
+++ lib/kubs/cli/version.rb
@@ -0,0 +1,5 @@
+module Kubs
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
---
+++ spec/kubs/cli_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Kubs::Cli do
+  it "has a version number" do
+    expect(Kubs::Cli::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
