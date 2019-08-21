009db4c6567ba4b37d81069ef6e4b6519553252d
paramagicdev <konnor5456@gmail.com>
2019-08-21 05:48:36 -0400

    Add some basic top level methods

--- lib/kubs_cli.rb
+++ lib/kubs_cli.rb
@@ -2,7 +2,25 @@
 
 require 'kubs_cli/version'
 
+# Top level class
 module KubsCLI
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Speciies class methods
+  class << self
+    attr_accessor :errors
+
+    # Adds an error to KubsCLI#errors
+    # @param e [Error]
+    def add_error(e:, msg: nil)
+      KubsCLI << e.exception(msg)
+    end
+
+    # Prints the errors when finished running
+    def print_errors
+      KubsCli << errors.each { |e| puts e.message }
+    end
+  end
+
+  @errors ||= []
 end
