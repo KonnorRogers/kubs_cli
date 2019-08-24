90ab090ad047e32fe3a1c134fcc40f3c3b6efd5c
paramagicdev <konnor5456@gmail.com>
2019-08-24 11:54:07 -0400

    update

--- lib/kubs_cli/pull.rb
+++ lib/kubs_cli/pull.rb
@@ -28,9 +28,7 @@ module KubsCLI
     end
 
     def copy_files(orig_file, new_file)
-      unless File.directory?(orig_file) || File.directory?(new_file)
-        return @fh.copy(from: orig_file, to: new_file)
-      end
+      return @fh.copy(from: orig_file, to: new_file) if File.directory?(orig_file)
 
       Dir.each_child(orig_file) do |o_file|
         Dir.each_child(new_file) do |n_file|
